a7ae2f252787f0220f9adc8aa20a79d829c2a46a
Jean Pierre Ortiz Murcia <ortizmurciajeanpierre@gmail.com>
2022-07-11 20:42:13 -0500

    adding sketchs

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -1,13 +1,13 @@
-import {
-  Sketch1,
-  Sketch2,
-  Sketch3,
-  Sketch4,
-  Sketch5,
-  Sketch6,
-  Sketch7,
-  Sketch8,
-} from "./sketchs";
+import { Sketch1 } from "./sketchs/sketch1";
+import { Sketch2 } from "./sketchs/sketch2";
+import { Sketch3 } from "./sketchs/sketch3";
+import { Sketch4 } from "./sketchs/sketch4";
+import { Sketch5 } from "./sketchs/sketch5";
+import { Sketch6 } from "./sketchs/sketch6";
+import { Sketch7 } from "./sketchs/sketch7";
+import { Sketch8 } from "./sketchs/sketch8";
+import { Sketch9 } from "./sketchs/sketch9";
+import { Sketch10 } from "./sketchs/sketch10";
 
 export const Sketchs = [
   Sketch1,
@@ -18,4 +18,6 @@ export const Sketchs = [
   Sketch6,
   Sketch7,
   Sketch8,
+  Sketch9,
+  Sketch10,
 ];
