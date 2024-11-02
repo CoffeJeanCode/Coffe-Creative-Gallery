0bc933d4f8b5cc9bac65bf50d9e4e4666642740f
Jean Pierre Ortiz <ortizmurciajeanpierre@gmail.com>
2024-11-02 17:49:50 -0500

    improve sketch 2

--- src/sketch/sketchs/sketch2/index.js
+++ src/sketch/sketchs/sketch2/index.js
@@ -1,28 +1,37 @@
 import SketchBase, { interactionPressed } from "../../sketchBase";
 
 export const Sketch2 = new SketchBase(
-  "Curve Lines",
+  "Vibrant Curve Lines",
   interactionPressed,
   (size) => (p) => {
     const path = [];
     let canvas;
+    const colors = ["#FF5733", "#33FF57", "#3357FF", "#F0FF33", "#FF33F6"]; // Paleta de colores
 
     p.setup = () => {
       canvas = p.createCanvas(size.width, size.height);
       p.noLoop();
       canvas.mouseOver(() => p.loop());
       canvas.mouseOut(() => p.noLoop());
+      p.smooth();
     };
 
     p.draw = () => {
-      p.background(0);
-      p.stroke(255);
+      p.background(0); // Fondo negro
+
+      // Dibuja líneas de guía
+      p.stroke(255, 50); // Color claro y translúcido
       p.line(p.mouseX, 0, p.mouseX, p.height);
       p.line(0, p.mouseY, p.width, p.mouseY);
+
       if (p.mouseIsPressed) {
         path.push(p.mouseX, p.mouseY);
       }
-      for (let i = 0; i < path.length; i++) {
+
+      // Dibuja líneas con colores alternos
+      for (let i = 0; i < path.length; i += 2) {
+        const colorIndex = (i / 2) % colors.length; // Alterna el índice del color
+        p.stroke(colors[colorIndex]); // Asigna el color
         p.line(path[i], p.mouseY, p.mouseX, path[i + 1]);
       }
     };
